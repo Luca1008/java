dbc8468ab6c9b87ff5011772aec6dbbdf6481d97
TuzioDj <tuzioxd@gmail.com>
2023-05-23 20:39:25 -0300

    TP Terminado

--- TP Especial Prog 3/src/GrafoDirigido.java
+++ TP Especial Prog 3/src/GrafoDirigido.java
@@ -13,15 +13,14 @@ public class GrafoDirigido<T> implements Grafo<T> {
     }
 
     /*
-     * La complejidad de la función 'agregarVertice' es O(1) en el mejor de los
-     * casos y O(n) en el peor de los casos.
-     * En el mejor de los casos, la función simplemente comprueba si el vértice ya
-     * existe en el HashMap y devuelve sin hacer nada más.
-     * Esto tiene una complejidad de tiempo constante O(1).
-     * En el peor de los casos, la función debe agregar un nuevo vértice al HashMap.
-     * Esto implica crear un nuevo objeto HashMap y agregarlo al HashMap principal.
-     * La complejidad de esto es O(n), donde n es el número de elementos en el
-     * HashMap.
+     COMPLEJIDAD: O(1) en el mejor de los casos y O(n) en el peor de los casos.
+     En el mejor de los casos, la función simplemente comprueba si el vértice ya
+     existe en el HashMap y devuelve sin hacer nada más.
+     Esto tiene una complejidad de tiempo constante O(1).
+     En el peor de los casos, la función debe agregar un nuevo vértice al HashMap.
+     Esto implica crear un nuevo objeto HashMap y agregarlo al HashMap principal.
+     La complejidad de esto es O(n), donde n es el número de elementos en el
+     HashMap.
      */
     public void agregarVertice(int verticeId) {
         // controlo que el vertice no exista
@@ -32,44 +31,34 @@ public class GrafoDirigido<T> implements Grafo<T> {
     }
 
     /*
-     * La complejidad de la función ‘borrarVertice’ es O(1) en el mejor de los casos
-     * y O(n) en el peor de los casos.
-     * En el mejor de los casos, la función simplemente elimina un elemento del
-     * HashMap y devuelve sin hacer nada más.
-     * Esto tiene una complejidad de tiempo constante O(1).
-     * En el peor de los casos, la función debe eliminar un elemento del HashMap
-     * y luego recorrer todo el HashMap para eliminar cualquier referencia a ese
-     * elemento(Osea sus arcos).
-     * La complejidad de esto es O(n), donde n es el número de elementos en el
-     * HashMap.
+     COMPLEJIDAD: O(V^2), donde V refiere a vertices, ya que primero buscamos el 
+     vertice, que esto tiene una complejidad O(1), y despues para eliminar los 
+     arcos que apuntan hacia el vertice objetivo, recorremos los vertices y sus
+     adyacentes para eliminar los arcos correspondientes.
+     Por lo tanto, en el peor de los casos deberiamos recorrer todos los vertices y
+     sus adyacentes, lo que lleva a una dificultad de O(V^2).
+     Por ultimo, borramos el vertice objetivo, lo cual tiene una dificultad O(1)
      */
-
     @Override
     public void borrarVertice(int verticeB) {
-        // borro vertice
-        vertices.remove(verticeB);
-        // for (int verticeId : this.vertices.keySet()) {
-        //     for (int destinoId : this.vertices.get(verticeId).keySet()) {
-        //         if (verticeB == destinoId) {
-        //             // borro arco
-        //             this.vertices.get(verticeId).remove(destinoId);
-        //             System.out.println("True");
-        
-        //         };
-        //     }
-        // }
-        
-        // al borrar vertice tendria q borrar todos los arcos q apuntan a el vertice borrado pero no nos sale
-        //se nos rompe y no sabemos por q 
+        if (vertices.containsKey(verticeB)) {
+            // Eliminar los arcos que apuntan hacia el vértice
+            for (HashMap<Integer, Arco<T>> adyacentes : vertices.values()) {
+                adyacentes.remove(verticeB);
+            }
+
+            // Eliminar el vértice del mapa de vértices
+            vertices.remove(verticeB);
+        }
     }
 
-    @Override
     /*
-     * COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
-     * put, donde coloca el arco al vertice obtenido(O(1))
-     * Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
-     * en el grafo.
-     */
+     COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
+     put, donde coloca el arco al vertice obtenido(O(1))
+     Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
+     en el grafo.
+    */
+    @Override
     public void agregarArco(int verticeOrigen, int verticeDestino, T etiqueta) {
         // controlo que el vertice no exista
         if (this.contieneVertice(verticeOrigen)) {
@@ -78,47 +67,44 @@ public class GrafoDirigido<T> implements Grafo<T> {
             // Buscamos el vertice y agregamos el arco
             vertices.get(verticeOrigen).put(verticeDestino, auxArco);
         }
-
     }
 
-    @Override
     /*
-     * COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
-     * remove, donde elimina el arco que se encuentre entre el vertice origen y
-     * destino.
-     * Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
-     * en el grafo.
-     */
+     COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
+     remove, donde elimina el arco que se encuentre entre el vertice origen y
+     destino.
+     Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
+     en el grafo.
+    */
+    @Override
     public void borrarArco(int verticeOrigen, int verticeDestino) {
         // Buscamos el arco y lo borramos
         vertices.get(verticeOrigen).remove(verticeDestino);
 
     }
 
-    @Override
     /*
-     * COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
-     * containsKey, que no recorre "vertices", sino que utiliza una función hash
-     * para calcular el índice en la tabla donde se almacenaría el elemento con esa
-     * clave.
-     * Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
-     * en el grafo.
-     */
+     COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
+     containsKey, que no recorre "vertices", sino que utiliza una función hash
+     para calcular el índice en la tabla donde se almacenaría el elemento con esa
+     clave.
+     Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
+     en el grafo.
+    */
+    @Override
     public boolean contieneVertice(int verticeId) {
         // Buscamos el vertice y devuelve si existe
         boolean contains = vertices.containsKey(verticeId);
         return contains;
     }
 
-    @Override
     /*
-     * COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
-     * containsKey, que no recorre "vertices", sino que utiliza una función hash
-     * para calcular el índice en la tabla donde se almacenaría el elemento con esa
-     * clave.
-     * Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
-     * en el grafo.
-     */
+     COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen (O(1)) y
+     containsKey, que no recorre "vertices", sino que utiliza una función hash
+     para calcular el índice en la tabla donde se almacenaría el elemento con esa clave.
+     Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices en el grafo.
+    */
+    @Override
     public boolean existeArco(int verticeOrigen, int verticeDestino) {
         // Buscamos el arco y devuelve si existe
         if (vertices.containsKey(verticeOrigen)) {
@@ -134,32 +120,38 @@ public class GrafoDirigido<T> implements Grafo<T> {
 
     }
 
-    @Override
     /*
-     * COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen y el
-     * vertice destino del arco para obtenerlo (O(1))
-     * Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
-     * en el grafo.
-     */
+     COMPLEJIDAD: O(1) Porque hace get, donde obtiene el vertice origen y el
+     vertice destino del arco para obtenerlo (O(1))
+     Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
+     en el grafo.
+    */
+    @Override
     public Arco<T> obtenerArco(int verticeOrigen, int verticeDestino) {
         Arco<T> aux = vertices.get(verticeOrigen).get(verticeDestino);
         return aux;
     }
 
-    @Override
     /*
-     * COMPLEJIDAD: O(1) Porque hace size, donde obtiene los numeros de elementos
-     * que obtiene el Hash
-     * Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
-     * en el grafo.
-     */
+     COMPLEJIDAD: O(1) Porque hace size, donde obtiene los numeros de elementos
+     que obtiene el Hash
+     Por lo tanto, el tiempo no aumenta a medida que aumenta el numero de vertices
+     en el grafo.
+    */
+    @Override
     public int cantidadVertices() {
         int aux = vertices.size();
         return aux;
     }
 
+    /*
+     COMPLEJIDAD: O(V), donde V son los vertices, ya que iteramos en el hashmap
+     de vertices, lo cual tendria una dificultad de O(V). Despues, obtenemos
+     el tamaño de los adyacentes.
+     Por ultimo, sumamos los tamaños de los adyacentes, accion la cual la hacemos
+     numero de vertices veces, lo cual seria una dificultad O(v)
+    */   
     @Override
-    // COMPLEJIDAD: O()
     public int cantidadArcos() {
         int aux = 0;
         for (Map.Entry<Integer, HashMap<Integer, Arco<T>>> entry : vertices.entrySet()) {
@@ -169,22 +161,35 @@ public class GrafoDirigido<T> implements Grafo<T> {
         return aux;
     }
 
+    /*     
+     COMPLEJIDAD: O(V), donde V son los vertices, ya que iteramos en el hashmap
+     de vertices para obtenerlos y guardarlos en una variable para posteriormente
+     retornarla.
+    */
     @Override
-    // COMPLEJIDAD: O()
     public Iterator<Integer> obtenerVertices() {
         Iterator<Integer> iterator = vertices.keySet().iterator();
         return iterator;
     }
 
+    /*     
+     COMPLEJIDAD: O(A), donde A son los arcos, ya que primero traemos el
+     vertice para despues iterar en los arcos  para obtener los adyacentes,
+     guardarlos en una variable y luego retornarla.
+     retornarla.
+    */
     @Override
-    // COMPLEJIDAD: O()
     public Iterator<Integer> obtenerAdyacentes(int verticeId) {
         Iterator<Integer> iterator = vertices.get(verticeId).keySet().iterator();
         return iterator;
     }
 
+    /*     
+     COMPLEJIDAD: O(V+A), donde V son los vertices y A son los arcos, ya
+     que tenemos que iterar entre los vertices, y por cada vertice iterar
+     sobre sus arcos para guardarlos y despues retornalos.
+    */
     @Override
-    // COMPLEJIDAD: O()
     public Iterator<Arco<T>> obtenerArcos() {
         List<Arco<T>> arcos = new ArrayList<>();
         for (Map.Entry<Integer, HashMap<Integer, Arco<T>>> entry : vertices.entrySet()) {
@@ -193,8 +198,12 @@ public class GrafoDirigido<T> implements Grafo<T> {
         return arcos.iterator();
     }
 
+    /*     
+     COMPLEJIDAD: O(A), donde A son los arcos, ya que primero traemos el
+     vertice objetivo para despues iterar sobre sus arcos, despues
+     guardarlos en una variable y luego retornarlos.
+    */
     @Override
-    // COMPLEJIDAD: O()
     public Iterator<Arco<T>> obtenerArcos(int verticeId) {
         Iterator<Arco<T>> iterator = vertices.get(verticeId).values().iterator();
         return iterator;
@@ -209,5 +218,4 @@ public class GrafoDirigido<T> implements Grafo<T> {
         }
         return super.toString();
     }
-
 }
